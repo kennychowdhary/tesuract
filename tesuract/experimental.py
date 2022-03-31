950dd466f42e814b096babad3e6f36b727a22fe3
Kamaljit Chowdhary <kennychowdhary@gmail.com>
2022-03-31 10:42:16 -0700

    added documentation and example usage to GPReg class

--- tesuract/experimental.py
+++ tesuract/experimental.py
@@ -13,6 +13,7 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from functools import partial
 from multiprocessing import Pool
+import multiprocessing as mp
 from scipy.optimize import fmin_l_bfgs_b
 import pickle, pdb
 from tqdm import tqdm
@@ -69,11 +70,8 @@ def csolve(K, y):
     return scipy.linalg.cho_solve((L, True), y), L
 
 
-# mtx_solve_mse = np.mean((npsolve(K,y) - csolve(K,y)[0])**2)
-
-
 def nplml(theta, X, y, kernel, nugget=1e-10):
-    """Compute the log marginal likelihood using numpy's mechanisms"""
+    """Compute the log marginal likelihood using numpy default solve (slower than cholesky)"""
     kernel_clone = kernel.clone_with_theta(theta)
     K = kernel_clone(X)
     K[np.diag_indices_from(K)] += nugget
@@ -87,7 +85,10 @@ def nplml(theta, X, y, kernel, nugget=1e-10):
 
 
 def clml(theta, X, y, kernel, nugget=1e-10, eval_gradient=False, return_K=False):
-    """Compute the log marginal likelihood using Cholesky decomposition"""
+    """Compute the log marginal likelihood using Cholesky decomposition
+
+    Modified and adapted from sklearn's GP regression class
+    """
     kernel_clone = kernel.clone_with_theta(theta)
     if eval_gradient:
         K, K_gradient = kernel_clone(X, eval_gradient=True)
@@ -189,7 +190,31 @@ class GPReg:
         How many times to solve the optimization of the log marginal likelihood. If using ElasticNetCV or LassoCV, might want to use the default otherwise it could be very slow. Also, since we are using multiprocessing, it is important to
 
     random_state: int, default=None
-        Random state 
+        Random state for choosing the initial guess in the random restarts for optimization
+
+    fit_params: dict, default=None
+        Dictionary to give the fit type. E.g., if using sklearn's RidgeCV, you can specify any of the algorithm options. See sklearn's documentation for all the lists of possible fit parameters
+
+    fit_type: {"RidgeCV", "LassoCV", "ElasticNetCV"}, default="RidgeCV"
+        fit algorithm used for solving the least squares solution to the polynomial coefficients. Unless the problem is zero noise, it is always better to use a regularized least squares solution. RidgeCV is the fastest of the three, but the others allow l-1 or sparsity constraints which may be useful.
+
+    Examples
+    --------
+    Given X which is n x d and Y which is n x R and an initial guess of the GP kernel, an a polynomial of order 7 using ElasticNetCV regularization, we can construct the GP+PCE regressor as follows.
+
+    >>> gppce = GPReg(kernel=kernel_guess,order=7,poly_alg='Legendre',input_range=None,n_restarts_optimizer=0,random_state=np.random.RandomState(23423),fit_type='ElasticNetCV',fit_params={'n_jobs':-1})
+
+    We can then fit it by simply calling
+    >>> gppce.fit(X,Y)
+
+    and return the predicted polynomial mean and GP standard deviation and covariance
+
+    >>> gppce_mu_pred, gppce_std_pred = gppce.predict(X,return_std=True)
+    >>> _, gppce_cov_pred = gppce.predict(X,return_cov=True)
+
+    Notes
+    -----
+    Due to problems with compatibility with OPENBLAS and the multiprocessing package, it might be necessary to set the environmental variable "export OPENBLAS_NUM_THREADS=1" before running the GPReg fit function.
 
     """
 
@@ -245,58 +270,30 @@ class GPReg:
         self.all_bounds = self.theta_bounds + self.coef_bounds
         return self
 
-    def log_marginal_likelihood(
-        self,
-        theta,
-        coef=None,
-        nugget=1e-6,
-        eval_gradient=False,
-        coef_bounds=(-2.0, 2.0),
-    ):
-        if coef is None:
-            self.coef = np.zeros(self.coef_size)
-            self.y_minus_mu = self.y
-        else:
-            assert (
-                len(coef) == self.coef_size
-            ), "coefficient array is not the corrent size."
-            self.y_minus_mu = self.y - np.dot(self.Xhat, coef)[:, np.newaxis]
-            # return list of bounds
-            self.coef_bounds = [coef_bounds for c in coef]
-        # evaluate log marginal likelihood
-        assert (
-            self.compile_flag == True
-        ), "Must compile first to precompute polynomial transform and save X,y data."
-        assert len(theta) == self.theta_size, "theta wrong length."
-        loglike = clml(
-            theta,
-            self.X,
-            self.y_minus_mu,
-            self.kernel,
-            nugget=nugget,
-            eval_gradient=eval_gradient,
-        )
-        return loglike
+    def log_marginal_likelihood(self, theta, nugget=1e-8, return_coef=False):
+        """Compute the log marginal likelihood of the data using the kernel hyper parameters and polynomial
 
-    # test optimize
-    def _objfunc(self, x0, fix_coef=False, coef=None, minimize=True):
-        l2reg = self.alpha
-        theta0 = x0[: self.theta_size]
-        if fix_coef:
-            coef0 = coef.copy()
-        else:
-            coef0 = x0[self.theta_size :]
-        mfact = 1.0
-        if minimize:
-            mfact = -1.0
-        lml = self.log_marginal_likelihood(theta0, coef0)
-        if l2reg is not None:
-            lml += -l2reg * np.sum(coef0 ** 2)
-            # lml += - l2reg*np.sqrt(np.sum(coef0**2+1e-6)) # l1
-        lml = mfact * lml
-        return lml
+        We use the formulation as outlined in https://www.mathworks.com/help/stats/exact-gpr-method.html. Note there is some uncertainty and confusion about the sigma variable in matlab's documentation and the white noise kernel parameter in sklearn's formulation. In this code and in sklearn's, if we add a white noise kernel to the full kernel, the log marginal likelihood is formulated the same as MATLAB's. The difference is in the prediction. Here, since we are separating out the stochastic/ GP from the polynomial model, it may not matter, but if we were to use MATLAB's posterior prediction, we have to modify the predict function.
 
-    def log_marginal_likelihood2(self, theta, nugget=1e-8, return_coef=False):
+        The log marginal likelihood can be written purely as a function of the kernel hyper parameters since the polynomial coefficient sub-problem can be written as a least squares problem with an exact OLS solution. This means that we do not have to search over the space of polynomial coefficients, avoiding, in part, the curse of dimensionality.
+
+        Parameters
+        ----------
+        theta : np.ndarray
+            kernel hyper-paramaters as computed using sklearn's kernel structure. Note that this structure is very confusing, so we try to leave it untouched to avoid bugs.
+
+        nugget : float, default=1e-8
+            Component to add to diagonal to avoid convergence issues with condition numbers. Make this number larger if having problems with Cholesky solves.
+
+        return_coef : bool, default=False
+            returns the polynomial coefficients. For use in debugging, but internally it is needed for the alternating minimization approach outlined in the MATLAB link above where we then solve the polynomial least squares problem.
+
+        Returns
+        -------
+        float
+            returns the scalar number which represents the log marginal likelihood of the data, given the polynomial and kernel theta hyper-parameters
+
+        """
         # compute pce coefficient first and then use the coef to evaulate lml
         kernel_clone = self.kernel.clone_with_theta(theta)
         K = kernel_clone(self.X)
@@ -326,18 +323,15 @@ class GPReg:
             self.fit_type = "RidgeCV"
             self.fit_params = ridgecv_fit_params
 
-        # reg = eval(name + "()") # way to convert string to function call
         if self.fit_type == "RidgeCV":
             regr = RidgeCV(fit_intercept=False, **self.fit_params)
         elif self.fit_type == "ElasticNetCV":
             regr = ElasticNetCV(fit_intercept=False, **self.fit_params)
         elif self.fit_type == "LassoCV":
             regr = LassoCV(fit_intercept=False, **self.fit_params)
-        # regr = ElasticNetCV(cv=2,random_state=0,tol=1e-4,max_iter=100000)
-        # regr = LassoCV(cv=2,random_state=0,tol=1e-3,max_iter=10000)
+
         regr.fit(Xtilde, ytilde)
         coef = regr.coef_
-        # print("coef2", coef2[:5])
         #######################
 
         self.y_minus_mu = self.y - np.dot(self.Xhat, coef)[:, np.newaxis]
@@ -357,27 +351,51 @@ class GPReg:
         else:
             return log_likelihood
 
-    def _objfunc2(self, theta, minimize=True):
-        # theta
-        # if fix_coef:
-        #   coef0 = coef.copy()
-        # else:
-        #   coef0 = x0[self.theta_size:]
+    def _objfunc(self, theta, minimize=True):
+        """Wrapper for the log marginal likelihood to turn the problem into a minimization instead of a maximization.
+
+        Parameters
+        ----------
+        theta : np.ndarray
+            kernel hyper-parameters. Note that the polynomial coefficients are completely determined by the kernel hyper-parameters using an OLS analytic solution (see the MATLAB link above for the formulation).
+
+        minimize : bool, default=True
+            Turns the log marginal likelihood objective into a minimization problem by adding a factor of -1 if True, and 1 if False (for maximization). The latter may be used for a Bayesian formulation where the log likelihood is needed in its maximization form.
+
+        Returns
+        -------
+        float
+            Returns the log marginal likelihood
+        """
         mfact = 1.0
         if minimize:
             mfact = -1.0
-        lml = self.log_marginal_likelihood2(theta)
-        # if l2reg is not None:
-        #   lml += -l2reg*np.sum(coef0**2)
-        # lml += - l2reg*np.sqrt(np.sum(coef0**2+1e-6)) # l1
+        lml = self.log_marginal_likelihood(theta)
         lml = mfact * lml
         return lml
 
-    def fit2(self, X, y):
+    def fit(self, X, y, **fit_params):
+        """Main fit routine for determining the GP kernel hyper-parameters and polynomial coefficients for the stochastic/ repeated data X, Y
+
+        Fitting a Polynomial + GP to the data pair X,y where y can have repeated elements. In general, it is difficult to deal with repeated data in a standard sense since this means your data matrix would have to have repeated rows, thus reducing the conditioning of your system and making the GP approach much more difficult (it scales like O(n^3) where n is the total number of samples). This routine essentially fits one GP model using a log marginal likelihood that averages the error out over the repeated samples. If you were to just fit a deterministic regression model to the averaged data, you would obtain a good mean approximation but loose information about the stochastic component. This helps avoid that.
+
+        Parameters
+        ----------
+        X : np.ndarray
+            Feature space data matrix, n x feature_dimensions, where n is the number of spatial samples
+        y : np.ndarray
+            Can be a matrix of size n x R, where R is the number of repeated outputs. Since this is meant for stochastic code, each realization of the simulation can result in slightly different target values so y can be a matrix, where the columns are the repeated samples.
+
+        Returns
+        -------
+        self
+            returns the GPReg object itself
+
+        """
         self._compile(X, y)
         opt_args = {"factr": 1e7, "disp": False}
         myfun = partial(
-            minimize_call, objfunc=self._objfunc2, bounds=self.theta_bounds, **opt_args
+            minimize_call, objfunc=self._objfunc, bounds=self.theta_bounds, **opt_args
         )
         nsolves = self.n_restarts_optimizer + 1
         # xstarts = [np.zeros(len(self.all_bounds)) for i in range(nsolves)]
@@ -394,7 +412,7 @@ class GPReg:
         # print(xstarts)
         print("Starting parallel optimization ...")
         start = T.time()
-        with Pool() as pool:
+        with mp.get_context("fork").Pool() as pool:
             res = pool.map(myfun, [x for x in xstarts])
         end = T.time()
         # print("Total time for optimization is %.5f seconds." %(end-start))
@@ -405,142 +423,10 @@ class GPReg:
         self.max_logp_ = -1 * fvalues[min_index]
         self.xmap_ = xvalues[min_index]
         self.theta_ = self.xmap_  # separate theta
-        logl, self.coef_ = self.log_marginal_likelihood2(
+        logl, self.coef_ = self.log_marginal_likelihood(
             self.theta_, return_coef=True
         )  # self.xmap_[self.theta_size:]   # separate pce coef
         return self
-
-    def fit(self, X, y):
-        self._compile(X, y)
-        opt_args = {"factr": 1e2, "disp": 25}
-        myfun = partial(
-            minimize_call, objfunc=self._objfunc, bounds=self.all_bounds, **opt_args
-        )
-        nsolves = self.n_restarts_optimizer + 1
-        # xstarts = [np.zeros(len(self.all_bounds)) for i in range(nsolves)]
-        if self.random_state is None:
-            self.random_state = np.random.RandomState(997)
-        ##########
-        start_bounds = self.theta_bounds + [
-            (-100, 100.0) for c in range(self.coef_size)
-        ]
-        xstarts = random_sample_within_bounds(nsolves, start_bounds)
-        # add solver for pce coef only to improve problem of poor fit when data not scaled.
-        ##########
-        # xstarts = [2*self.random_state.rand(len(self.all_bounds))-1 for i in range(nsolves)]
-        # print(xstarts)
-        print("Starting parallel optimization ...")
-        start = T.time()
-        with Pool() as pool:
-            res = pool.map(myfun, [x for x in xstarts])
-        end = T.time()
-        print("Total time for optimization is %.5f seconds." % (end - start))
-
-        xvalues = [r[0] for r in res]
-        fvalues = [r[1] for r in res]
-        min_index = np.argmin(fvalues)
-        self.max_logp_ = -1 * fvalues[min_index]
-        self.xmap_ = xvalues[min_index]
-        self.theta_ = self.xmap_[: self.theta_size]  # separate theta
-        self.coef_ = self.xmap_[self.theta_size :]  # separate pce coef
-        return self
-
-    def fit_bayesian(self, X, y):
-        self._compile(X, y)
-
-        def log_like(x):
-            return self._objfunc(x, minimize=False)
-
-        def log_prob(x):
-            bc = check_array_within_bounds(x, self.all_bounds)
-            if np.all(bc):
-                return log_like(x)
-            else:
-                return -1e12
-
-        nwalkers = 100
-        nburn = 1500
-        nruns = 500
-        ndim = len(self.all_bounds)
-        start_bounds = self.theta_bounds + [(-1, 1) for c in range(self.coef_size)]
-        p0 = random_sample_within_bounds(nwalkers, start_bounds)
-        # print([log_prob(xi) for xi in p0])
-
-        # start emcee sampler
-        sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob)
-        state = sampler.run_mcmc(p0, nburn, progress=True)  # burn in
-        sampler.reset()
-        sampler.run_mcmc(state, nruns, progress=True)  # main run
-        try:
-            acor_times = sampler.get_autocorr_time()
-            print(acor_times)
-            thin = round(acor_times.mean() / 2)
-        except:
-            print("Cannot get acor times. Chain too short. ")
-            thin = 1
-
-        import matplotlib.pyplot as plt
-
-        samples = sampler.get_chain(flat=True, thin=thin)
-        logprobs = sampler.get_log_prob(flat=True)
-        argmap = np.argmax(logprobs)
-        map_soln = samples[argmap]
-        self.xmap_bayes_ = map_soln
-        self.theta_ = self.xmap_bayes_[: self.theta_size]  # separate theta
-        self.coef_ = self.xmap_bayes_[self.theta_size :]  # separate pce coef
-
-        self.samples_ = samples
-        self.logprobs_ = logprobs
-        self.thetas_ = samples[:, : self.theta_size]
-        self.coefs_ = samples[:, self.theta_size :]
-
-        map_logprob = logprobs[argmap]
-        for nd in range(ndim):
-            fig, ax = mpl.subplots()
-            ax.hist(samples[:, nd], 100, color="k", histtype="step")
-            fig.savefig("mcmc_hist_plot_dim" + str(nd + 1).zfill(2) + ".png")
-
-        fig = corner.corner(samples, truths=map_soln, labels=self.feature_labels)
-        fig.savefig("corner_plot.png")
-
-        return self
-
-    def predict_bayes(
-        self,
-        X,
-        return_pce_only=False,
-        return_std=False,
-        return_cov=False,
-        return_samples=None,
-    ):
-        # plot the test prediction using the GP PCE model
-        predictions = []
-        pce_predictions = []
-        nsamples = len(self.samples_)
-        plot_thin = round(nsamples / 200)
-        for i in tqdm(range(0, nsamples, plot_thin)):
-            y_mu_gppce_pred = self.pce.polyeval(X, c=self.coefs_[i])
-            kernel_opt_ = self.kernel.clone_with_theta(self.thetas_[i])
-            rn = self.random_state
-            gp = GaussianProcessRegressor(kernel=kernel_opt_, random_state=rn)
-            gp_mu_resid, gp_std_resid = gp.predict(X, return_std=True)
-            _, gp_cov_resid = gp.predict(X, return_cov=True)
-            pred = y_mu_gppce_pred + gp_mu_resid
-            predictions.append(pred)
-            pce_predictions.append(y_mu_gppce_pred)
-        # if return_std is True:
-        #   return y_mu_gppce_pred + gp_mu_resid, gp_std_resid
-        # elif return_cov is True:
-        #   return y_mu_gppce_pred + gp_mu_resid, gp_cov_resid
-        # elif return_pce_only is True:
-        #   return y_mu_gppce_pred, self.pce
-        # if isinstance(return_samples,int):
-        #   nsamples = return_samples
-        #   gp_resid_samples = gp.sample_y(X,nsamples)
-        #   return y_mu_gppce_pred[:,np.newaxis] + gp_resid_samples
-        # else:
-        #   return y_mu_gppce_pred + gp_mu_resid
-        return np.array(predictions), np.array(pce_predictions)
 
     def predict(
         self,

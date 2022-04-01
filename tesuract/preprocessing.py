7e7e7d0ca984eaaf145903ab977b0a09689f3d62
Kamaljit Chowdhary <kennychowdhary@gmail.com>
2022-04-01 14:03:28 -0700

    changed the default cutoff for PCA

--- tesuract/preprocessing.py
+++ tesuract/preprocessing.py
@@ -178,7 +178,7 @@ class MinMaxTargetScaler:
 
 class PCATargetTransform(BaseEstimator, TransformerMixin):
     def __init__(
-        self, n_components=2, exp_var_cutoff=0.99, svd_solver="arpack", whiten=False
+        self, n_components=2, exp_var_cutoff=0.90, svd_solver="arpack", whiten=False
     ):
         self.n_components = n_components
         self.exp_var_cutoff = exp_var_cutoff
@@ -213,7 +213,8 @@ class PCATargetTransform(BaseEstimator, TransformerMixin):
         return self
 
     def fit_transform(self, Y):
-        return self.pca.fit_transform(Y)
+        self.fit(Y)
+        return self.pca.transform(Y)
 
     def transform(self, Y):
         assert hasattr(self, "pca"), "Perform fit first."

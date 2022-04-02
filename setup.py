ddfd8ef95e0504925365513fdc4325fd2fd46e13
Kamaljit Chowdhary <kennychowdhary@gmail.com>
2022-04-02 15:10:26 -0700

    changed version to 0.1.2

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 from setuptools import setup
 
+
 def readme():
-    with open('README.rst') as f:
+    with open("README.rst") as f:
         return f.read()
 
-setup(name='tesuract',
-      version='1.0.1',
-      description='Tensor surrogate construction methods',
-      long_description='UQ and ML tools for surrogate construction',
-      url='https://github.com/kennychowdhary/tesuract',
-      author='Kenny Chowdhary',
-      author_email='kchowdh@sandia.gov',
-      license='BSD3',
-      packages=['tesuract'],
-      test_suite='nose.collector',
-      tests_required=['nose'],
-      install_requires=[
-          'numpy',
-          'sklearn',
-          'tqdm'
-      ],
-      include_package_data=True,
-      zip_safe=False)
+
+setup(
+    name="tesuract",
+    version="0.1.2",
+    description="Tensor surrogate construction methods",
+    long_description="UQ and ML tools for surrogate construction",
+    url="https://github.com/kennychowdhary/tesuract",
+    author="Kenny Chowdhary",
+    author_email="kchowdh@sandia.gov",
+    license="BSD3",
+    packages=["tesuract"],
+    test_suite="nose.collector",
+    tests_required=["nose"],
+    install_requires=["numpy", "sklearn", "tqdm"],
+    include_package_data=True,
+    zip_safe=False,
+)
